a2f8529739889ae505cfabd3ab5e1dec7c22d96f
ArceFacundo <17LuckGames17@gmail.com>
2024-04-10 18:49:46 -0300

    Fondo lluvia de estrellas

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+export default function Home() {
+  return (
+    <main className="w-full fondo-de-estrellas">
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+      <span className="star"></span>
+    </main>
+  );
+}
